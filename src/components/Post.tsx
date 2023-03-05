3ff6a80702f90591551f0fbe38005c4a406f86fe
MuhammedAli Faidi <mouhammedalifaidi@gmail.com>
2023-03-05 09:02:52 +0100

    deploy

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -11,6 +11,7 @@ export default function Posts({ post }: PostProps) {
 						className="cursor-pointer w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
 						type="checkbox" name="published" id="published"
             checked={published}
+            onChange={() =>console.log('change')}
 					
 					/>
 					<label htmlFor="published" className={`cursor-pointer}`}>
